6475a9609703dcd4a1bc8857b945224ceb532c2e
André luiz azevedo <andreazevedoluiz@gmail.com>
2023-02-15 14:02:39 -0300

    Criando a classe Elenco e  as Dtos read e create

---
+++ MegaFilmesAPI/MegaFilmesAPI/Data/Dtos/CreateElencoDtocs.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MegaFilmesAPI.Data.Dtos
+{
+    public class CreateElencoDtocs
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Name { get; set; }
+    }
+}
---
+++ MegaFilmesAPI/MegaFilmesAPI/Data/Dtos/ReadElencoDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MegaFilmesAPI.Data.Dtos
+{
+    public class ReadElencoDto
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Name { get; set; }
+    }
+}
