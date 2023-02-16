0c9422ddf098dbd8f4581941da0b26517b4b3222
André luiz azevedo <andreazevedoluiz@gmail.com>
2023-02-15 22:03:57 -0300

    Auterações

--- MegaFilmesAPI/MegaFilmesAPI/Controllers/ElencoController.cs
+++ MegaFilmesAPI/MegaFilmesAPI/Controllers/ElencoController.cs
@@ -1,6 +1,48 @@
-﻿namespace MegaFilmesAPI.Controllers
+﻿using AutoMapper;
+using MegaFilmesAPI.Data;
+using MegaFilmesAPI.Data.Dtos;
+using MegaFilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegaFilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ElencoController :ControllerBase
 {
-    public class ElencoController
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public ElencoController(FilmeContext context, IMapper mapper)
     {
+        _context = context;
+        _mapper = mapper;
     }
+
+    [HttpPost]
+    public IActionResult AdicionaElenco([FromBody] CreateElencoDto elencoDto)
+    {
+        Elenco elenco = _mapper.Map<Elenco>(elencoDto);
+        _context.Elencos.Add(elenco);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperarElencoPorId), new { Id = elenco.Id }, elenco);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperarElencoPorId(int id)
+    {
+        var elenco = _context.Elencos.Find(id);
+
+        if (elenco != null)
+        {
+            ReadElencoDto elencoDto = _mapper.Map<ReadElencoDto>(elenco);
+            return Ok();
+        }
+
+        return NotFound();
+    }
+
+
+
+
 }
